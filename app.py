2676aab96903721167a119164bf7bb6562117888
Josh Wilkie <josh@rawt.ca>
2017-10-28 11:59:48 -0700

    Fixed Card borders and spacing, Added dynamic css loading to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from data import CardSearch
+import os
 
 Cards = CardSearch()
 
@@ -9,6 +10,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('archiveTrap.html', cards = Cards)
+
+@app.context_processor
+def overide_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 if __name__ == '__main__':
 	app.run(debug=True)
