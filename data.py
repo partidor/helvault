5318c42996fd0b6b93cf50925169bee89778622d
Josh Wilkie <josh@rawt.ca>
2017-11-06 14:44:36 -0800

    Added name change to Helvault, added large image on click feature

--- data.py
+++ data.py
@@ -45,14 +45,31 @@ def CardSearch(q):
 		cimage_small = ""
 		cimage_large = ""
 		ot = ""
-		try:
-			ot = c['oracle_text']
-		except:
-			pass
+		
+		if 'oracle_text' in c:
+			if c['mana_cost'] == "":
+				l1 = c['name']
+			else:
+				l1 = c['name'] + ' - ' + c['mana_cost'] 
+			l2 = c['type_line']
+			ot =  l1 + '<br><br>' + l2 + '<br><br>' + c['oracle_text']
+		else:
+			try:
+				text = c['card_faces'][0]['name'] + ' - ' + c['card_faces'][0]['mana_cost'] + '<br><br>' + c['card_faces'][0]['type_line'] + '<br><br>' + c['card_faces'][0]['oracle_text']
+				text = text + '<br><br>' + c['card_faces'][1]['name'] + '<br><br>' + c['card_faces'][1]['type_line'] + '<br><br>' + c['card_faces'][1]['oracle_text']
+				ot = text
+			except:
+				pass
+
+
 		if 'card_faces' in c:
-			#print "DOUBLE FACE ALERT"
-			cimage_small = c['card_faces'][0]['image_uris']['normal']
-			cimage_large = c['card_faces'][0]['image_uris']['large']
+			if 'image_uris' in c['card_faces'][0]:
+				print "DOUBLE FACE ALERT"
+				cimage_small = c['card_faces'][0]['image_uris']['normal']
+				cimage_large = c['card_faces'][0]['image_uris']['large']
+			else:
+				cimage_small = c['image_uris']['normal']
+				cimage_small = c['image_uris']['large']
 		else:
 			cimage_small = c['image_uris']['normal']
 			cimage_small = c['image_uris']['large']
--- static/javascript/cardSelect.js
+++ static/javascript/cardSelect.js
@@ -3,4 +3,7 @@ $('.card-click').on('click',function(){
 	var oracle_text = $(this).attr('oracle');
 	$('#oracle-text-box').html(oracle_text);
 
+	var large_img = $(this).attr('src');
+	$('#large-image-box').attr('src', large_img);
+
 });
