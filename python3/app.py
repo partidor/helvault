6057d1a35ea8077aecfb162f2b15bac0d435aaf1
Josh Wilkie <josh@rawt.ca>
2017-11-20 16:55:07 -0800

    Added python 3 compatability

---
+++ python3/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for, request
+from data import CardSearch
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        #search_string = request.form['search_string']
+        #Process search string -> url for Scryfall API
+        #Cards = CardSearch('')
+        #return render_template('archiveTrap.html')
+        return render_template('archiveTrap.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+        search_string = request.args.get('jsdata')
+        #print search_string
+        #Process search string -> url for Scryfall API
+        Cards = CardSearch(search_string)
+        return render_template('cards.html', cards = Cards)
+
+@app.context_processor
+def overide_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+if __name__ == '__main__':
+        app.run(debug=True)
---
+++ python3/data.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import json
+from urllib.parse import urlparse
+from urllib.parse import urlunsplit
+from urllib.parse import quote
+from urllib.request import urlopen
+
+class Card:
+	name = ""
+	set_name = ""
+	oracle_text = ""
+	s_img = None
+	l_img = None
+
+	def __init__(self, name, set_name, oracle_text, s_img, l_img):
+		self.name = name
+		self.set_name = set_name
+		self.oracle_text = oracle_text
+		self.s_img = s_img
+		self.l_img = l_img
+
+#def CardSearch(url):
+def CardSearch(q):
+	cardList = []
+
+	url="https://api.scryfall.com/cards/search?q=c%3Awhite+cmc%3D1+t%3Acreature+f%3Apauper"
+	parse = urlparse(url)
+
+	user_url = quote(q, safe='')
+	user_url= user_url.replace('%20', '+')
+	user_url = 'q=' + user_url
+
+	url = urlunsplit((parse.scheme, parse.netloc, parse.path, user_url, parse.fragment))
+
+	#print url
+
+	resp = urlopen(url)
+
+	data = json.loads(resp.read().decode('utf-8'))
+
+	#print json.dumps(data, indent=4)
+	if data['object'] == "error":
+		#print 'ERROR'
+		return cardList
+
+	for c in data['data']:
+		cimage_small = ""
+		cimage_large = ""
+		ot = ""
+
+		if 'oracle_text' in c:
+			if c['mana_cost'] == "":
+				l1 = c['name']
+			else:
+				l1 = c['name'] + ' - ' + c['mana_cost']
+			l2 = c['type_line']
+			ot =  l1 + '<br><br>' + l2 + '<br><br>' + c['oracle_text']
+		else:
+			try:
+				text = c['card_faces'][0]['name'] + ' - ' + c['card_faces'][0]['mana_cost'] + '<br><br>' + c['card_faces'][0]['type_line'] + '<br><br>' + c['card_faces'][0]['oracle_text']
+				text = text + '<br><br>' + c['card_faces'][1]['name'] + '<br><br>' + c['card_faces'][1]['type_line'] + '<br><br>' + c['card_faces'][1]['oracle_text']
+				ot = text
+			except:
+				pass
+
+
+		if 'card_faces' in c:
+			if 'image_uris' in c['card_faces'][0]:
+				#print "DOUBLE FACE ALERT"
+				cimage_small = c['card_faces'][0]['image_uris']['normal']
+				cimage_large = c['card_faces'][0]['image_uris']['png']
+				aimage_small = c['card_faces'][1]['image_uris']['normal']
+				aimage_large = c['card_faces'][1]['image_uris']['png']
+
+				n = Card(c['name'], c['set'], ot, cimage_small, cimage_large)
+				cardList.append(n)
+
+				a = Card(c['name'], c['set'], ot, aimage_small, aimage_large)
+				cardList.append(a)
+				continue
+			else:
+				cimage_small = c['image_uris']['normal']
+				cimage_large = c['image_uris']['png']
+		else:
+			cimage_small = c['image_uris']['normal']
+			cimage_large = c['image_uris']['png']
+
+		n = Card(c['name'], c['set'], ot, cimage_small, cimage_large)
+		cardList.append(n)
+
+	##for c in cardList:
+	##	print c.name, c.set_name, c.oracle_text, c.s_img, c.l_img
+
+	return cardList
+
+if __name__ == '__main__':
+	print(CardSearch("vance's blasting cannons"))
